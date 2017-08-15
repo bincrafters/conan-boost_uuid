10abedbf71d8fbaaf838b0915c4f52d13b198a7b
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:23 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostUuidConan(ConanFile):
     name = "Boost.Uuid"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/uuid"
+    url = "https://github.com/bincrafters/conan-boost-uuid"
+    source_url = "https://github.com/boostorg/uuid"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "uuid"
@@ -25,7 +25,7 @@ class BoostUuidConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
