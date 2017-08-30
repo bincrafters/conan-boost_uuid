5288e8d0652cffc8ff05d18c875355471fab068b
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:28:14 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostUuidConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-uuid"
-    source_url = "https://github.com/boostorg/uuid"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["uuid"]
@@ -25,9 +24,12 @@ class BoostUuidConan(ConanFile):
                       #assert1 config0 core2 io1 iterator5 random9 serialization11 smart_ptr4 static_assert1 throw_exception2 type_traits3 winapi1
                       
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
